c6229768b99f342bb47f19ea2934183ead17c1b7
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2023-04-27 17:34:31 -0300

    Parece que nao basta adicionar o SQLAlchemy pelo pip, tem que adicionar ao PyCharm na GUI.

--- MapeamentoObjetoRelacional.py
+++ MapeamentoObjetoRelacional.py
@@ -13,3 +13,15 @@ from sqlalchemy.ext.automap import automap_base
 # Ligação com o DB feita por um objeto motor -engine- do SQLAlchemy ele que realiza o trabalho de conectar e mapear
 motor=create_engine("mysql+mysqlconnector://pedro:senha@localhost/db_escola?charset=utf8mb4")
 # estrutura URL:  ([tipoDB]+[tipoConnector]://[nomeUsuário]:[senha]@[localacesso]/[nomeSchema]?[outrasInfo])
+
+# Mapeando o BD para os Objetos
+DB=automap_base() # cria um objeto que acessa o BD
+DB.prepare(motor,reflect=True) # acessa o BD usando o motor
+## agora sim mapeia as tabelas para objetos
+aluno=DB.classes.aluno
+disciplina=DB.classes.disciplina
+matricula=DB.classes.matricula
+
+print(type(aluno))
+
+
