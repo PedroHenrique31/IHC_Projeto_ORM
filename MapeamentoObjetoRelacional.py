863d30f1da88b47d1c19f9bce7267ee85340e662
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2023-04-27 17:01:54 -0300

    Adicionado objeto motor, que realizara o mapeamento e no SQLAlchemy isso e feito de um jeito muito interessante

--- MapeamentoObjetoRelacional.py
+++ MapeamentoObjetoRelacional.py
@@ -9,3 +9,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
 
+# ------- Variáveis globais ---------------------------------------------------
+# Ligação com o DB feita por um objeto motor -engine- do SQLAlchemy ele que realiza o trabalho de conectar e mapear
+motor=create_engine("mysql+mysqlconnector://pedro:senha@localhost/db_escola?charset=utf8mb4")
+# estrutura URL:  ([tipoDB]+[tipoConnector]://[nomeUsuário]:[senha]@[localacesso]/[nomeSchema]?[outrasInfo])
