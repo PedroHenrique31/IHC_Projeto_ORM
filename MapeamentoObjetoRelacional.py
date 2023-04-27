d300d25c06b0e365561243a5355e60ef93b38f10
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2023-04-27 16:50:43 -0300

    Commit inicial do projeto onde quero treinar ORM usando Python.

---
+++ MapeamentoObjetoRelacional.py
@@ -0,0 +1,11 @@
+"""
+Author: Pedro Henrique Carneiro de Araújo
+
+    Treinando mapeamento Objeto-Relacional
+    Vou copiar uns codigos aqui, mpra entender como usar o SQLAlchemy.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.automap import automap_base
+
