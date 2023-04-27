4f40a341d2757a7e3f44e7e75b3933dc3e34b6f1
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2023-04-27 18:37:52 -0300

    Faltou adicionar uma dependencia de um pacote chamado cryptography, para conectar-se ao MySQL.

--- MapeamentoObjetoRelacional.py
+++ MapeamentoObjetoRelacional.py
@@ -11,17 +11,17 @@ from sqlalchemy.ext.automap import automap_base
 
 # ------- Variáveis globais ---------------------------------------------------
 # Ligação com o DB feita por um objeto motor -engine- do SQLAlchemy ele que realiza o trabalho de conectar e mapear
-motor=create_engine("mysql+mysqlconnector://pedro:senha@localhost/db_escola?charset=utf8mb4")
+motor=create_engine("mysql+pymysql://pedro:senhaTosca@localhost/db_escola?charset=utf8mb4")
 # estrutura URL:  ([tipoDB]+[tipoConnector]://[nomeUsuário]:[senha]@[localacesso]/[nomeSchema]?[outrasInfo])
 
 # Mapeando o BD para os Objetos
 DB=automap_base() # cria um objeto que acessa o BD
-DB.prepare(motor,reflect=True) # acessa o BD usando o motor
+DB.prepare(autoload_with=motor) # acessa o BD usando o motor
 ## agora sim mapeia as tabelas para objetos
 aluno=DB.classes.aluno
 disciplina=DB.classes.disciplina
 matricula=DB.classes.matricula
 
-print(type(aluno))
+#print(type(aluno)) # tá... queria ver a classe aí, mas fiquei mais confuso
 
 
